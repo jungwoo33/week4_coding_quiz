8844c59639b554aa1f905ca6511e3e0afd245ed1
Jungwoo Lee <jungwoo33@gmail.com>
2022-11-14 16:20:18 -0600

    v7 updated & readme is now updated

---
+++ my_questions.js
@@ -0,0 +1,105 @@
+const my_questions = [
+    {
+        question: '1. Javascript is an ____ Language?',
+        choices:[
+            'Object-Oriented',
+            'Ojbect-Based',
+            'Procedural',
+            'None of the above'
+        ],
+        correct_answer: '1'
+    },
+    {
+        question: '2. Which of the following keywords is used to define a variable in Javascript?',
+        choices:[
+            'var',
+            'let',
+            'Both A and B',
+            'None of the above'
+        ],
+        correct_answer: '3'
+    },
+    {
+        question: '3. Which of the following method is used to access HTML elements using Javascript?',
+        choices:[
+            'getElementByID()',
+            'getElementByClassName()',
+            'Both A and B',
+            'None of the above'
+        ],
+        correct_answer: '3'
+    },
+    {
+        question: '4. Upon encountering empty statements, what does the Javascript Interpreter do?',
+        choices:[
+            'Throws an error',
+            'Ignores the statements',
+            'Gives a warning',
+            'None of the above'
+        ],
+        correct_answer: '2'
+    },
+    {
+        question: '5. Which of the following methods can be used to display data in some form using Javascript?',
+        choices:[
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+            'All of the above'
+        ],
+        correct_answer: '4'
+    },
+    {
+        question: '6. How can a datatype be declared to be a constant type?',
+        choices:[
+            'const',
+            'var',
+            'let',
+            'constant'
+        ],
+        correct_answer: '1'
+    },
+    {
+        question: '7. When the switch statement matches the expression with the given labels, how is the comparison done?',
+        choices:[
+            'Both the datatype and the results of the expression are compared.',
+            'Only the datatype of the expression is compared.',
+            'Only the value of the expression is compared.',
+            'None of the above'
+        ],
+        correct_answer: '1'
+    },
+    {
+        question: '8. What keyword is used to check whether a given property is valid or not?',
+        choices:[
+            'in',
+            'is in',
+            'exists',
+            'lies'
+        ],
+        correct_answer: '1'
+    },
+    {
+        question: '9. What is the use of the <noscript> tag in Javascript?',
+        choices:[
+            'The contents are displayed by non-JS-based browsers.',
+            'Clears all the cookies and cache.',
+            'Both A and B',
+            'None of the above'
+        ],
+        correct_answer: '1'
+    },
+    {
+        question: '10. Commonly used data types DO NOT include:',
+        choices:[
+            'strings',
+            'booleans',
+            'alerts',
+            'numbers'
+        ],
+        correct_answer: '3'
+    }
+]
+
+// exporting variables:
+export {my_questions};
